8ccedb2b982bf8ab9a1ec1dc27ec5778695ba20e
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-05 02:02:48 +0000

    'sign up' page initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:friday_coins/screens/signup/signup.dart';
 import 'package:friday_coins/screens/splash/splash.dart';
 import 'package:friday_coins/signin/signin.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SignIn(),
+    home: SignUp(),
     debugShowCheckedModeBanner: false,
   ));
 }
