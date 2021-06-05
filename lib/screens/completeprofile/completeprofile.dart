cb811f772e5d2f45b95c2ca03eac9f21cc7aff07
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-05 13:44:07 +0000

    'complete profile' initialised

---
+++ lib/screens/completeprofile/completeprofile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CompleteProfile extends StatefulWidget {
+
+  @override
+  _CompleteProfileState createState() => _CompleteProfileState();
+}
+
+class _CompleteProfileState extends State<CompleteProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
